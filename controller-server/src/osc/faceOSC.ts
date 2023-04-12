e0b4ae1236499b9bfa8f1ce5251697ad4234e7ca
Gunn Keem <gunnkeem@icloud.com>
2023-04-12 16:07:50 +0900

    ALL WORKING FINE!

--- controller-server/src/osc/faceOSC.ts
+++ controller-server/src/osc/faceOSC.ts
@@ -1,37 +1,14 @@
 import { Server } from 'node-osc';
-import servoController from "../arduino/serials/servoController.js";
-
-export const FACE_OSC_PORT = 12300;
-export const FACE_OSC_WIDTH = 640;
-export const FACE_OSC_HEIGHT = 480;
-
-function handleMessagePack(address: string, messagePack: Array<string>) {
-  switch (address) {
-
-    case '/found':
-      break;
-
-    case '/pose/position':
-      servoController.sendFacePosition(messagePack);
-    break;
-
-    default:
-      break;
-  }
-}
+import serialPort from '../arduino/serialPort.js';
 
 export function useFaceOSCstreamline(oscServer: Server) {
 
-  oscServer.on('bundle', (bundle) => {
-    bundle.elements.forEach((element, i) => {
-      const messagePack = element.toString().split(',');
-      const address = messagePack.splice(0, 1)[0];
-      handleMessagePack(address, messagePack);
-    })
-  })
-
   oscServer.on('message', (msg) => {
-    console.log(`Message: ${msg}`);
     const [address, ...args] = msg;
+    
+    if (address === '/mappedAngle') {
+      const angle = args[0] as number;
+      serialPort.write(Buffer.from([angle]));
+    }
   })
 }
