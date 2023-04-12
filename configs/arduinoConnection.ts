fe12a2714df9a3a96eef29e3fff2e66c3f3551ca
Gunn Keem <gunnkeem@icloud.com>
2023-04-12 14:48:59 +0900

    RPI - Arudino setup complete!!!

--- configs/arduinoConnection.ts
+++ configs/arduinoConnection.ts
@@ -1,4 +1,9 @@
 export default {
-  SERIAL_PORT_PATH: "/dev/cu.usbmodem1101",
+
+  // Macbook Air - the lower port
+  // SERIAL_PORT_PATH: "/dev/cu.usbmodem1101",
+
+  // Raspberry Pi 4B - the upper port of USB3 (Blue ports)
+  SERIAL_PORT_PATH: "/dev/ttyACM0",
 
 }
